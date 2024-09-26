8d29d54545cb69c3555801a8aab4cb8539f90fa8
Anthony Li Perera <anthony.li.perera@est.una.ac.cr>
2024-09-26 14:53:11 -0600

    Update Linea.java

--- src/main/java/pos/logic/Linea.java
+++ src/main/java/pos/logic/Linea.java
@@ -1,8 +1,11 @@
 package pos.logic;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Linea {
     private Producto producto;
     private int cantidad;
